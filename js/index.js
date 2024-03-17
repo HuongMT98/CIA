ae53930708e5a0807bd2f6a9cde76cb523997a95
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2024-03-17 17:03:43 +0700

    Carrers

--- js/index.js
+++ js/index.js
@@ -10,7 +10,9 @@ function closeNavBar() {
 
 // 🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥🚥
 
+
 /*NavMenu-Option */
+
 
 let showNavbar = document.querySelectorAll('.navmenu-option-lg-m');
 showNavbar.forEach(e => {
@@ -19,3 +21,56 @@ showNavbar.forEach(e => {
         navShow.classList.toggle('open-navbar')
     });
 });
+
+
+// Swiper Library
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+//                                  Scroll Hidden Navbar
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
+//                                  Navbar Mobile
+
+
+let dropMenu = document.querySelector('.navbar-mobile')
+
+
+function closeMenuNav() {
+    dropMenu.classList.toggle('active');
+}
+function openMenuNav() {
+    dropMenu.classList.toggle('active');
+}
